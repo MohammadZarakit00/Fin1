ee61652403eb96890d35130177523d8e16a70f05
Filip Olsson <fi3126ol-s@lu.se>
2020-12-11 12:54:07 +0100

    Added check for studentId in Student constructor, also test cases in StudentTest to try it.

--- src/model/StudentTest.java
+++ src/model/StudentTest.java
@@ -3,6 +3,10 @@ package model;
 public class StudentTest {
 
         public static void main(String[] args){
+            Student s1 = new Student("S12345","Filip");
+            Student s2 = new Student("345667", "John");
+
+
             System.out.println("En student är snäll");
         }
 }
