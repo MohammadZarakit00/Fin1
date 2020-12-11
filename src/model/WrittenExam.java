846da5437f30c3fb2331add3968660db0c0b43de
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-11 13:44:30 +0100

    Update WrittenExam.java

--- src/model/WrittenExam.java
+++ src/model/WrittenExam.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class WrittenExam {
 	
 	private String examID;
@@ -10,46 +12,52 @@ public class WrittenExam {
 	
 	private String time;
 	
-	private double maxPoints;
+	private double maxPoints = 100;
+	
+	public ArrayList<Student> sList = new ArrayList<Student>();
 
 	public String getExamID() {
 		return examID;
 	}
-
 	public void setExamID(String examID) {
 		this.examID = examID;
 	}
-
 	public String getDate() {
 		return date;
 	}
-
 	public void setDate(String date) {
 		this.date = date;
 	}
-
 	public String getLocation() {
 		return location;
 	}
-
 	public void setLocation(String location) {
 		this.location = location;
 	}
-
 	public String getTime() {
 		return time;
 	}
-
 	public void setTime(String time) {
 		this.time = time;
 	}
-
 	public double getMaxPoints() {
 		return maxPoints;
 	}
-
 	public void setMaxPoints(double maxPoints) {
 		this.maxPoints = maxPoints;
+	}
+	
+	public void addStudent(Student s) {
+		sList.add(s);
+	}
+	
+	public Student findStudent(Student studentId) {
+		for (Student tmpId : sList) {
+			if (sList.contains(tmpId)) {
+				return studentId;
+			}
+		}
+		return null;
 	}
 
 }
