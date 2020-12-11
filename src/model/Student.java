f084d75eb07e2c36b59ed2ce7079514d61101863
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-11 12:20:09 +0100

    person-methods

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,8 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Student {
 	
+	private ArrayList<Student> sList = new ArrayList<Student>();
+	
 	private String studentId;
+	
+	private String name;
 	
 	public String getStudentId() {
 		return studentId;
@@ -19,7 +25,27 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	private String name;
+	
+	public void addStudent(Student s) {
+		sList.add(s);
+	}
+	
+	public Student findStudent(String studentId) {
+		for (Student tmpS : sList) {
+			if (tmpS != null) {
+				return tmpS;
+			}
+		}
+		return null;
+	}
+	
+	public Student deleteStudent(String studentId) {
+		if (sList.contains(findStudent(studentId))) {
+			sList.remove(findStudent(studentId));
+			System.out.println(studentId + " was deleted from the system.");
+		}
+		return null;
+	}
+	
 
 }
