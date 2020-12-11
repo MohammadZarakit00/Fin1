bf84d8f833cbc5293ff9818c2dc19ef28091ddff
Filip Olsson <fi3126ol-s@lu.se>
2020-12-11 16:01:50 +0100

    Changed letterGrade from char to string since we have to return "Fail" and not only chars. Also implemented method which generates letterGrade from a result.

--- src/model/Result.java
+++ src/model/Result.java
@@ -2,24 +2,51 @@ package model;
 
 public class Result {
 	
-	private String result;
+	private int result;
 	
-	private char letterGrade;
+	private String letterGrade;
 
-	public String getResult() {
+	public Result(int result){
+		if(result >= 0 && result <= 100){
+			this.result = result;
+			this.letterGrade = letterGradeFromResult(result);
+		}
+	}
+
+	public int getResult() {
 		return result;
 	}
 
-	public void setResult(String result) {
+	public void setResult(int result) {
 		this.result = result;
 	}
 
-	public char getLetterGrade() {
+	public String getLetterGrade() {
 		return letterGrade;
 	}
 
-	public void setLetterGrade(char letterGrade) {
+	public void setLetterGrade(String letterGrade) {
 		this.letterGrade = letterGrade;
 	}
 
+
+	/*
+	Kan vara så att det första konditionet i varje else-if-sats inte behövs då
+	checken sker redan i första och sedan "följer med".
+	 */
+	public String letterGradeFromResult(int result){
+		if(result < 50){
+			return "Fail";
+		} else if(result >= 50 && result < 55){
+			return "E";
+		} else if(result >= 55 && result < 65){
+			return "D";
+		} else if(result >= 65 && result < 75){
+			return "C";
+		} else if(result >= 75 && result <85){
+			return "B";
+		} else {
+			return "A"; //Behöver inte kollas då det görs i konstruktorn.
+		}
+	}
 }
--- src/model/StudentTest.java
+++ src/model/StudentTest.java
@@ -9,6 +9,18 @@ public class StudentTest {
             WrittenExam e1 = new WrittenExam("E12345");
             WrittenExam e2 = new WrittenExam("S123456");
 
-            System.out.println("En student är snäll");
+            Result r1 = new Result(0);
+            Result r2 = new Result(100);
+            Result r3 = new Result(65);
+            Result r4 = new Result(1337);
+            Result r5 = new Result(-1);
+
+            System.out.println("r1: " + r1.getLetterGrade());
+            System.out.println("r2 " + r2.getLetterGrade());
+            System.out.println("r3 " + r3.getLetterGrade());
+            System.out.println("r4 " + r4.getLetterGrade());
+            System.out.println("r5 " + r5.getLetterGrade());
+
+
         }
 }
