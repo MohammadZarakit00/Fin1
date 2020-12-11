dde9fbe0559d2d0598b6aa8c7beb2d7c32a3f9f6
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-11 11:42:57 +0100

    instansvariabler

---
+++ src/model/Course.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Course {
+
+	private String courseCode;
+	
+	private String name;
+	
+	private String credits;
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCredits() {
+		return credits;
+	}
+
+	public void setCredits(String credits) {
+		this.credits = credits;
+	}
+
+}
---
+++ src/model/WrittenExam.java
@@ -0,0 +1,55 @@
+package model;
+
+public class WrittenExam {
+	
+	private String examID;
+	
+	private String date;
+	
+	private String location;
+	
+	private String time;
+	
+	private double maxPoints;
+
+	public String getExamID() {
+		return examID;
+	}
+
+	public void setExamID(String examID) {
+		this.examID = examID;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public double getMaxPoints() {
+		return maxPoints;
+	}
+
+	public void setMaxPoints(double maxPoints) {
+		this.maxPoints = maxPoints;
+	}
+
+}
