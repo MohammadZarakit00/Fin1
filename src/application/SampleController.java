5a599cfd59b74e61cefe67fcd5add12a8f82e43f
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-14 16:58:01 +0100

    Update SampleController.java

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -28,41 +28,85 @@ import javafx.stage.Stage;
 
 public class SampleController {
 	
-	//private ArrayList<Student> studentRegister = new ArrayList<Student>();
-	private ArrayList<Course> courses = new ArrayList<>();
+	private ArrayList<Student> studentRegister = new ArrayList<Student>();
+	private ArrayList<Course> courseRegister = new ArrayList<Course>();
 	
 	@FXML
 	Button btnAddStudent = new Button();
 	@FXML
 	Button btnFindStudent = new Button();
 	@FXML
-	Button btnDeleteStudent = new Button();
+	Button btnRemoveStudent = new Button();
+	@FXML
+	Button btnAddCourse = new Button();
+	@FXML
+	Button btnFindCourse = new Button();
+	@FXML
+	Button btnRemoveCourse = new Button();
+	@FXML
+	Button btnAddExam = new Button();
 	@FXML
 	TextArea ta = new TextArea();
 	@FXML
 	TextField tfId = new TextField();
 	@FXML
 	TextField tfName = new TextField();
-
+	@FXML
+	TextField tfCredits = new TextField();
 	
 	@FXML
 	public void btnAddStudent(ActionEvent event) {
 		String tmpId = tfId.getText();
 		String tmpName = tfName.getText();
-		testExam.addStudent(new Student(tmpId, tmpName));
+		studentRegister.add(new Student(tmpId, tmpName));
+		ta.setText("Student" + " " + tmpId + " " + tmpName + " " + "was added to the system. "); 
 
 	}
 	
-	@FXML
-	public String btnFindStudent(ActionEvent event) {
+	public void btnRemoveStudent(ActionEvent event) {
 		String tmpId = tfId.getText();
-		if (tfId.getText().equals(tmpId)) {
-			ta.setText(tmpId);
-			
+		studentRegister.remove(tmpId);
+		ta.setText("Student " + tmpId + " was removed from the system. ");
+	}
+	
+	@FXML
+	public void btnAddCourse(ActionEvent event) {
+		String tmpId = tfId.getText();
+		String tmpName = tfName.getText();
+		try {
+			int tmpCredits = Integer.parseInt(tfCredits.getText());
+			courseRegister.add(new Course(tmpId, tmpName, tmpCredits));
+			ta.setText("Course " + tmpName + " with course code " + tmpId + " worth " + tmpCredits + " credits was added to the list.") ;
+		} catch (NumberFormatException e) {
+			ta.setText("Input is not accepted. Try a number. ");
+		}
+	}
+	
+	@FXML
+	public void btnRemoveCourse(ActionEvent event) {
+		String tmpId = tfId.getText();
+		courseRegister.remove(tmpId);
+		ta.setText(tmpId + " was removed from the register."); 
+		System.out.println("test");
+	}
+	
+	
+	
+	//test line please ignore 
+	
+	
+	
+/*@FXML
+	public String btnFindStudent(ActionEvent event) {
+		WrittenExam writtenExam = sList.findStudent(tfId.getText());
+		if (studentRegister.contains(student)) {
+			ta.setText(student.getName() + student.getStudentId());
 		}
 		return null;
-		
 	}
+	*/
+	
+	
 	
 }
 
