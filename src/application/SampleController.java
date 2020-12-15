86fc11b7209f4444606784eab2b96f7ad3c60340
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-15 11:25:54 +0100

    separate scenes draft 1
    currently nullpointer on getScene, known bug

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -27,6 +27,17 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SampleController {
+	
+	@FXML
+	Button btnStudentManagement = new Button();
+	
+	public void btnStudentManagement(ActionEvent event) throws Exception {
+		
+	Parent root = FXMLLoader.load(this.getClass().getResource("/Sample.fxml"));
+	Stage window = (Stage) btnStudentManagement.getScene().getWindow();
+	window.setScene(new Scene (root));
+	}
+	
 
 	private ArrayList<Student> studentRegister = new ArrayList<Student>();
 	private ArrayList<Course> courseRegister = new ArrayList<Course>();
