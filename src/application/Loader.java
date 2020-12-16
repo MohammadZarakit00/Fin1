5eec367cc2f64b0e6851882c4d2257f58e26cf62
john-sta <69900325+john-sta@users.noreply.github.com>
2020-12-16 13:27:08 +0100

    working sidebar menu
    wrong dimensions, currently overlaps the buttons so have to fix some kinda boundary.
    also new loader class to find the fxml-files

---
+++ src/application/Loader.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.net.URL;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+public class Loader {
+	
+	private Pane view;
+	
+	public Pane getPage(String fileName) {
+	try {
+		URL fileUrl = HomePageController.class.getResource(fileName +".fxml");
+		
+		if (fileUrl == null) {
+			throw new java.io.FileNotFoundException("File not found.");
+		}
+		
+		new FXMLLoader();
+		view = FXMLLoader.load(fileUrl);	
+		}
+	
+		catch (Exception e) {
+			System.out.println("no page");
+		}
+	return view;
+	}
+
+}
