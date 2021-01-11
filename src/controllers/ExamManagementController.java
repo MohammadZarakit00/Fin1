b157ddfc26341000b832b07ee3c04bc950cfe73f
john-sta <69900325+john-sta@users.noreply.github.com>
2021-01-11 15:50:35 +0100

    small fixes and attempt at adding exam results for a student

--- src/controllers/ExamManagementController.java
+++ src/controllers/ExamManagementController.java
@@ -16,6 +16,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import model.Course;
 import model.CourseRegister;
+import model.Student;
+import model.StudentRegister;
 import model.WrittenExam;
 import model.WrittenExamRegister;
 
@@ -25,17 +27,20 @@ public class ExamManagementController extends Controller implements Initializabl
 
 	private CourseRegister courseRegister = CourseRegister.getCourseRegInstance();
 	private WrittenExamRegister examRegister = WrittenExamRegister.getExamRegInstance();
+	private StudentRegister studentRegister = StudentRegister.getStudentRegInstance();
 
 	
 	
 	@FXML
 	ComboBox courseChoiceBox = new ComboBox();
 	@FXML
+	ComboBox studentChoiceBox = new ComboBox();
+	@FXML
 	Button btnGenerateExamId = new Button();
 	
 	
-//	@FXML
-//	Button btnAddExam = new Button();
+	@FXML
+	Button btnAddExam = new Button();
 	@FXML
 	Button btnRemoveExam = new Button();
 	@FXML
@@ -46,6 +51,8 @@ public class ExamManagementController extends Controller implements Initializabl
 	Button btnNbrPassedExam = new Button();
 	@FXML
 	Button btnAddStudentToExam = new Button();
+	@FXML
+	Button btnRegisterResult = new Button();
 
 	@FXML
 	TextField tfId = new TextField();
@@ -56,32 +63,39 @@ public class ExamManagementController extends Controller implements Initializabl
 	@FXML
 	TextField tfTime = new TextField();
 	@FXML
+	TextField tfScore = new TextField();
+	@FXML
 	TextArea outPutArea = new TextArea();
 	@FXML
 	Text errorText = new Text();
 	
-	
+	//Fills out drop down menus with courses and students respectively, to choose when adding exam to courses or registering results for students.
 	public void initialize(URL location, ResourceBundle resources){
 		System.out.println("Kör initialize");
-		ArrayList<String> tmpList = new ArrayList<>();
+		ArrayList<String> tmpCourseList = new ArrayList<>();
 		for(Course c : courseRegister.getCourseRegister()){
-			tmpList.add(c.getCourseCode() + ": " + c.getName());
+			tmpCourseList.add(c.getCourseCode() + ": " + c.getName());
 		}
-		courseChoiceBox.setItems(FXCollections.observableArrayList(tmpList));
+		courseChoiceBox.setItems(FXCollections.observableArrayList(tmpCourseList));
+		
+		ArrayList<String> tmpStudentList = new ArrayList<>();
+		for(Student s : studentRegister.getStudentRegister()) {
+			tmpStudentList.add(s.getStudentId() + ": " + s.getName());
+		}
+		studentChoiceBox.setItems(FXCollections.observableArrayList(tmpStudentList));
 	}
 
-
-	//Im missing something here, can't figure out how to add the exam TO the course...
-	/*@FXML
+	
+	@FXML
 	public void btnAddExam(ActionEvent event) {
 			String tmpIdExam = tfId.getText();
 			String tmpIdDate = tfDate.getText();
 			String tmpIdLocation = tfLocation.getText();
 			String tmpIdTime = tfTime.getText();
-			CourseRegister currentCourse = CourseRegister.getCourseRegInstance();
-		examRegister.add(new WrittenExam(tmpIdExam, tmpIdDate, tmpIdLocation, tmpIdTime, course)); //this one is wonk supreme. needs to define exam FOR the given course
-		outPutArea.setText("Exam " + tmpIdExam + " was added for the course " + currentCourse + ". ");
-	} */
+			Course currentCourse = (Course) courseChoiceBox.getValue();//almost..... but not quite. class cast exception, cant cast to Course
+		examRegister.add(new WrittenExam(tmpIdExam, tmpIdDate, tmpIdLocation, tmpIdTime, currentCourse)); //this one is wonk supreme. needs to define exam FOR the given course
+		outPutArea.setText("Exam " + tmpIdExam + " was added to the course " + currentCourse + ". ");
+	} 
 	
 	//Same as above. Remove FROM a course.
 	@FXML
@@ -113,34 +127,49 @@ public class ExamManagementController extends Controller implements Initializabl
 	@FXML
 	public void btnMedianResult(ActionEvent event) {
 		String tmpExamId = tfId.getText();
-		for (WrittenExam e : examRegister.getExamRegister()) {
-			if (e.getExamID().equals(tmpExamId)) {
-				outPutArea.setText("Median result for this exam is " + e.getMedianResult());
+		WrittenExam exam = examRegister.findExam(tmpExamId);
+			if (exam != null) {				
+				outPutArea.setText("Median result for this exam is " + exam.getMedianResult());
 			}
 			else {
 				outPutArea.setText("No exam exists with this ID. Please try another. ");
 				}
 			}
-		}
+		
 	
 	@FXML
 	public void btnNbrPassedExam(ActionEvent event) {
 		String tmpExamId = tfId.getText();
-		for (WrittenExam e : examRegister.getExamRegister()) {
-			if (e.getExamID().equals(tmpExamId)) {
-				outPutArea.setText(e.nbrPassedExam() + " passed the exam.");
+		WrittenExam exam = examRegister.findExam(tmpExamId);
+			if (exam != null) {
+				outPutArea.setText(exam.nbrPassedExam() + " passed the exam.");
 			}
 			else {
 				outPutArea.setText("No exam exists with this ID. Please try another. ");
 			}
 		}
+	
+	@FXML
+	public void btnRegisterResult(ActionEvent event) {
+		WrittenExam exam = null;
+		String tmpExamId = tfId.getText();
+		String tmpScore = tfScore.getText();
+		String tmpStudentId = (String) studentChoiceBox.getValue(); //cant cast, idkwtf to put here
+		if (tmpExamId == null || tmpScore == null || tmpStudentId == null) {
+			exam.setStudentResult(tmpStudentId, Integer.parseInt(tmpScore)); //gotta work in examId somehow.
+			outPutArea.setText("Score of " + tmpScore + " registered on the exam " + tmpExamId + " for student " + tmpStudentId);
+		} else {
+			outPutArea.setText("You must enter an exam ID, a score, and choose for which student you wish to register." );
+		}
 	}
+	
 		
 	@FXML
 	public void btnGenerateExamId(ActionEvent event) {
 		Random examGen = new Random();
 		int examId = examGen.nextInt(99999 + 1 - 10000) + 10000;
 		tfId.setText("E" + examId);
+		
 	}
 	
 	
